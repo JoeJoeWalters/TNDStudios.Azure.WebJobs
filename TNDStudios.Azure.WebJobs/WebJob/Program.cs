df1af90ea90bb07949202ae0a5d4108317a106c4
TNDStudios <info@thenakeddeveloper.com>
2021-02-01 00:14:31 +0000

    Continuous Webjob Example

--- TNDStudios.Azure.WebJobs/WebJob/Program.cs
+++ TNDStudios.Azure.WebJobs/WebJob/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WebJob
 {
@@ -6,7 +11,36 @@ namespace WebJob
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HostBuilder builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+                b.AddWarmup();
+            });
+            builder.ConfigureAppConfiguration(cb =>
+            {
+                cb.AddJsonFile("appsettings.json");
+            });
+            IHost host = builder.Build();
+            using (host)
+            {
+                host.StartAsync().Wait();
+                IJobHost jobHost = (IJobHost)host.Services.GetService(typeof(IJobHost));
+                jobHost.CallAsync(nameof(Functions.MyContiniousMethod)).Wait();
+            }
+        }
+    }
+
+    public class Functions
+    {
+        [NoAutomaticTrigger]
+        public void MyContiniousMethod()
+        {
+            while (true != false)
+            {
+                Thread.Sleep(1000);
+            }
         }
     }
 }
